6e0f28b4fdaef605c29a5a23f07274171c047306
Josip Digital <info@digriot.com>
2017-03-12 16:02:34 +0100

    Add project files.

---
+++ logicalsorting/script.js
@@ -0,0 +1,7 @@
+﻿var App = angular.module("App", ['ngDragDrop']);
+App.controller('OverviewCtrl', function ($scope) {
+    $scope.list1 = { title: 'good' };
+    $scope.list2 = { title: 'fat' };
+    $scope.list3 = { title: 'fluffy' };
+    $scope.list4 = {};
+});
